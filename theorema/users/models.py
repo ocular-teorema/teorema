46d42dbe82059f1cb9bd327b6f21cd02e20f15df
aleksei-udalov <aleksei.udalov.01@gmail.com>
2017-07-25 13:10:29 +0400

    rollback hashing passwords

--- theorema/users/models.py
+++ theorema/users/models.py
@@ -9,3 +9,14 @@ class User(AbstractUser):
     is_organization_admin = models.BooleanField(default=False)
     organization = models.ForeignKey(Organization, null=True)
     camera_group = models.ForeignKey(CameraGroup, null=True)
+
+
+# https://stackoverflow.com/questions/26786512/how-to-see-if-a-field-changed-in-model-save-method
+@receiver(models.signals.pre_save, sender=User)
+def hash_pass(sender, instance, **kwargs):
+    try:
+        current_instance = sender.objects.get(id=instance.id)
+        if current_instance.password != instance.password:
+            instance.set_password(instance.password)
+    except sender.DoesNotExist:
+        instance.set_password(instance.password)
