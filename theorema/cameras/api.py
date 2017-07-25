b72e6a7406e5dffb16e13414ef4ab9bbe6e20c09
aleksei-udalov <aleksei.udalov.01@gmail.com>
2017-07-25 13:01:15 +0400

    cameras filter

--- theorema/cameras/api.py
+++ theorema/cameras/api.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from .models import Server, Camera, CameraGroup
 from .serializers import ServerSerializer, CameraSerializer, CameraGroupSerializer
 
@@ -8,10 +9,18 @@ class ServerViewSet(ModelViewSet):
 
 
 class CameraViewSet(ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     queryset = Camera.objects.all()
     serializer_class = CameraSerializer
 
-
+    def get_queryset(self):
+        if not self.request.user.is_staff:
+            return self.queryset.filter(server__organization=self.request.user.organization)
+        param = self.request.query_params.get('organization', None)
+        if param is not None:
+            return self.queryset.filter(server__organization__id=param)
+        return self.queryset
+        
 class CameraGroupViewSet(ModelViewSet):
     queryset = CameraGroup.objects.all()
     serializer_class = CameraGroupSerializer
