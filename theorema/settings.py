3950f5f0206fd3f3cc17ba5814fdf99607bec1e8
sdeath666 <sdeath666@sdeath>
2018-03-14 15:46:27 +0300

    add redis/celery

--- theorema/settings.py
+++ theorema/settings.py
@@ -151,6 +151,14 @@ MEDIA_ROOT = os.path.join(ROOT, 'media/')
 MEDIA_URL = '/media/'
 ARCHDIR = '/home/_VideoArchive/'
 
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = '6379'
+BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
+CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+
+
 try:
     from theorema.settings_local import *
 except ImportError as exc:
