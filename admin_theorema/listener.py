0833c6f436699795a4273f2fc8f26cbd50693d2c
aleksei-udalov <aleksei.udalov.01@gmail.com>
2018-06-04 16:27:09 +0300

    fix

--- admin_theorema/listener.py
+++ admin_theorema/listener.py
@@ -122,7 +122,7 @@ def save_config(numeric_id, req):
             diff_analysis = 'true' if req['analysis'] > 1 else 'false',
             indefinitely='true' if req['indefinitely'] else 'false',
             janus_port=int(req['port'])+1,
-            archive_path =  req['arhcive_path'] if req['arhcive_path'] else'/home/_VideoArchive'
+            archive_path =  req['archive_path'] if req['archive_path'] else'/home/_VideoArchive'
         ))
 #        os.system('kill `lsof -i | grep janus | awk "{ print $2 }" | head -n1`')
 #        Popen(['/opt/janus/bin/janus'])
@@ -147,7 +147,7 @@ class Cam(Resource):
             with open("/opt/janus/etc/janus/janus.plugin.streaming.cfg", "a") as f:
                 f.write("\n[cam {id} restreaming sample]\ntype = rtp\ndescription = {id}\naudio = no\nvideo = yes\nvideoport = {port}\nvideopt = 96\nvideortpmap = H264/90000\nvideofmtp = profile-level-id=42e01f\n".format(id=req['id'], port=int(req['port'])+1))
                 f.close()
-                os.system('kill `lsof -i | grep janus | awk "{ print $2 }" | head -n1`')
+                os.system('kill `pidof janus`')
                 Popen(['/opt/janus/bin/janus'])
         except Exception as e:
             print('\n'.join(traceback.format_exception(*sys.exc_info())))
@@ -261,7 +261,7 @@ def launch_cameras():
             all_cams_info[cam]['process'] = None
 
 os.system('kill `pidof processInstance`')
-os.system('kill `lsof -i | grep janus | awk "{ print $2 }" | head -n1`')
+os.system('kill `pidof janus`')
 Popen(['/opt/janus/bin/janus'])
 
 lock = Lock()
