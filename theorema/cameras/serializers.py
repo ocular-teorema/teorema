5b22efe7943deb4bbb49614b2cedb1ed963b2988
sdeath666 <superficial.death@gmail.com>
2018-07-03 13:24:35 +0300

    theorema_offline_fix

--- theorema/cameras/serializers.py
+++ theorema/cameras/serializers.py
@@ -64,8 +64,8 @@ class CameraSerializer(M2MHelperSerializer):
         }
 
     def create(self, validated_data):
- #       if not OcularUser.objects.exists():
- #           raise APIException(code=400, detail={'status': 1, 'message': 'user doesn"t exist'})
+        if not OcularUser.objects.exists():
+            raise APIException(code=400, detail={'status': 1, 'message': 'user doesn"t exist'})
  #       max_cams=requests.post('http://78.46.97.176:1234/account', json={'hash':OcularUser.objects.last().hardware_hash})
 #        if Camera.objects.filter(analysis=validated_data['analysis']).count() == max_cams.json()['шmax_cams'][CAM_TYPES[validated_data['analysis']]]:
 #            raise APIException(code=400, detail={'status': 1, 'message': 'no pain - no gain'})
