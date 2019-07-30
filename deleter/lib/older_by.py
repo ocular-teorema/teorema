6505b17aa11ca42de673b673db4f9ad39fb01c32
Maksim Strenk <ephdtrg@gmail.com>
2019-07-30 13:58:13 +0300

    fix imports

--- deleter/lib/older_by.py
+++ deleter/lib/older_by.py
@@ -1,6 +1,6 @@
 import os
 import datetime
-from file_size import return_name,create_name_path,find_files_older,return_name,convert_name_to_datetime
+from deleter.lib.file_size import return_name,create_name_path,find_files_older,return_name,convert_name_to_datetime
 
 
 def day_limit():
