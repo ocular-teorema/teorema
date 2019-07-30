fc896c7d4058698d7e075528a236c41cbdddf534
Evgeniy Gazetdinov <zplacesound@gmail.com>
2019-07-30 12:44:37 +0300

    some Update delete_handler.py

--- deleter/lib/delete_handler.py
+++ deleter/lib/delete_handler.py
@@ -15,11 +15,21 @@ def find_free_space():
 
 def delete_handler(files, limit,middle_file,ratio,free_space = 1,):
     # count how many files need deleted
-    counter = 0
+       counter = 0
     free_space = find_free_space()
-    while free_space< limit:
-           free_space = find_free_space()
-           delete_file(files[counter])
-           print(counter)
-           counter+1
-           print(str(files[counter]) + ' file will be deleted')
+    print(str(len(files))+'files for delete')
+    print(free_space)
+    delete_paths = []
+    for i in files:
+        for key, value in i.items():
+            delete_paths.append(value)
+    try:
+        while free_space< limit:
+            free_space = find_free_space()
+            #delete_file(files[counter])
+            print(counter)
+            counter+1
+            print(str(delete_paths[counter])+ ' file will be deleted')
+    except:
+         print('files'+str(counter)+'deleted')
+         pass
