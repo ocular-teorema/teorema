b50ecd72c0b42199f6593fab5c2b21ac119ab01f
Euphemia Deathridge <ephdtrg@gmail.com>
2019-10-09 14:56:32 +0300

    cut microseconds

--- admin_theorema/listener.py
+++ admin_theorema/listener.py
@@ -471,8 +471,8 @@ class ArchiveVideo(Resource):
         start_date_db = ' and date >= ' + str(DateTime(start_dt_date.replace('-', '/') + ' UTC').JulianDay())
         end_date_db = ' and date <= ' + str(DateTime(end_dt_date.replace('-', '/') + ' UTC').JulianDay())
 
-        start_posix_time = ' and  start_posix_time >=' + str(start_time)
-        end_posix_time = ' and end_posix_time <=' + str(end_time)
+        start_posix_time = ' and  start_posix_time >=' + str(start_time)[:-3]
+        end_posix_time = ' and end_posix_time <=' + str(end_time)[:-3]
 
         conn = psycopg2.connect(host='localhost', dbname='video_analytics', user='va', password='theorema')
         cur = conn.cursor()
