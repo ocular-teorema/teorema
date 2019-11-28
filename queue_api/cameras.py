7705d4cfd909b1334e41308fa6c26adddca51d8a
Egor14 <egorgrekhnev@mail.ru>
2019-11-28 19:26:25 +0300

    stop and start cameras with their updating

--- queue_api/cameras.py
+++ queue_api/cameras.py
@@ -4,7 +4,6 @@ from admin_theorema import listener
 
 
 class CameraListMessages(QueueEndpoint):
-
     camera_group = 'default'
     organization = 'Ocular'
 
@@ -13,11 +12,11 @@ class CameraListMessages(QueueEndpoint):
 
         name = params['name']
         address_primary = params['address_primary']
-        #address_secondary = params['address_secondary']
+        # address_secondary = params['address_secondary']
         analysis_type = params['analysis_type']
         storage_days = params['storage_days']
-        #storage_id = params['storage_id']
-        #schedule_id = params['schedule_id']
+        # storage_id = params['storage_id']
+        # schedule_id = params['schedule_id']
 
         serializer_params = {
             'name': name,
@@ -55,10 +54,36 @@ class CameraListMessages(QueueEndpoint):
 
         camera = Camera.objects.filter(uid=params['id']).first()
         if camera:
+            data = {
+                'address': camera.address,
+                'analysis': camera.analysis,
+                'archive_path': camera.archive_path,
+                'camera_group': camera.camera_group.id,
+                'compress_level': camera.compress_level,
+                'id': camera.id,
+                'indefinitely': camera.indefinitely,
+                'is_active': False,
+                'm3u8_video_url': 'http://%s:8080/vasrc/cam%s/index.m3u8' % (camera.server.address, str(camera.id)),
+                'name': camera.name,
+                'notify_alert_level': camera.notify_alert_level,
+                'notify_email': camera.notify_email,
+                'notify_events': camera.notify_events,
+                'notify_phone': camera.notify_phone,
+                'notify_send_email': camera.notify_send_email,
+                'notify_send_sms': camera.notify_send_sms,
+                'notify_time_start': camera.notify_time_start,
+                'notify_time_stop': camera.notify_time_stop,
+                'organization': camera.organization.id,
+                'port': camera.port,
+                'rtmp_video_url': 'rtmp://%s:1935/vasrc/cam%s' % (camera.server.address, str(camera.id) + camera.add_time),
+                'server': camera.server.id,
+                'storage_life': camera.storage_life,
+                'thumb_url': 'http://%s:5005/thumb/%s/' % (camera.server.address, camera.id),
+                'unique_id': str(camera.id) + camera.add_time,
+                'ws_video_url': 'ws://%s/video_ws/?port=%s' % (camera.server.address, camera.port+50)
+            }
             try:
-                listener.del_autostart(params['id'])
-                camera.is_active = False
-                camera.save()
+                CameraSerializer().update(camera, data)
             except:
                 raise Exception('fail in stopping camera')
 
@@ -74,13 +99,38 @@ class CameraListMessages(QueueEndpoint):
 
         camera = Camera.objects.filter(uid=params['id']).first()
         if camera:
+            data = {
+                'address': camera.address,
+                'analysis': camera.analysis,
+                'archive_path': camera.archive_path,
+                'camera_group': camera.camera_group,
+                'compress_level': camera.compress_level,
+                'id': camera.id,
+                'indefinitely': camera.indefinitely,
+                'is_active': True,
+                'm3u8_video_url': 'http://%s:8080/vasrc/cam%s/index.m3u8' % (camera.server.address, str(camera.id)),
+                'name': camera.name,
+                'notify_alert_level': camera.notify_alert_level,
+                'notify_email': camera.notify_email,
+                'notify_events': camera.notify_events,
+                'notify_phone': camera.notify_phone,
+                'notify_send_email': camera.notify_send_email,
+                'notify_send_sms': camera.notify_send_sms,
+                'notify_time_start': camera.notify_time_start,
+                'notify_time_stop': camera.notify_time_stop,
+                'organization': camera.organization,
+                'port': camera.port,
+                'rtmp_video_url': 'rtmp://%s:1935/vasrc/cam%s' % (camera.server.address, str(camera.id) + camera.add_time),
+                'server': camera.server,
+                'storage_life': camera.storage_life,
+                'thumb_url': 'http://%s:5005/thumb/%s/' % (camera.server.address, camera.id),
+                'unique_id': str(camera.id) + camera.add_time,
+                'ws_video_url': 'ws://%s/video_ws/?port=%s' % (camera.server.address, camera.port+50)
+            }
             try:
-                path = listener.get_path(params['id'])
-                listener.add_autostart('cam', params['id'], path)
-                camera.is_active = True
-                camera.save()
+                CameraSerializer().update(camera, data)
             except:
-                raise Exception('fail in starting camera')
+                raise Exception('fail in stopping camera')
 
         message = {
             'request_uid': params['request_uid'],
