3eba7c17394303b45e687e9953cdfc4f28c6b706
Euphemia Deathridge <ephdtrg@gmail.com>
2019-12-06 15:09:51 +0300

    fix archive

--- queue_api/archive.py
+++ queue_api/archive.py
@@ -22,17 +22,27 @@ class VideosGetMessage(ArchiveQueueEndpoint):
         print('message received', flush=True)
         self.uuid = params['uuid']
         print('request uid', self.uuid, flush=True)
-        print('params', params['data'], flush=True)
+
+        data = params['data']
+        print('params', data, flush=True)
 
         if self.check_request_params(params['data']):
             return
 
+        camera_list = data['cameras']
+        camera_list_query = []
+        for camera in camera_list:
+            camera = camera[3:]
+            camera_list_query.append(camera)
+
+        camera_query = ','.join(camera_list_query)
+
         query_params = {
-            'startTs': int(params['data']['start_timestamp']) * 1000,
-            'endTs': int(params['data']['stop_timestamp']) * 1000,
-            'cameras': ','.join(params['data']['cameras']),
-            'skip': 0 if 'skip' not in params['data'].keys() else params['data']['skip'],
-            'limit': 10000 if 'limit' not in params['data'].keys() else params['data']['limit']
+            'startTs': int(data['start_timestamp']) * 1000,
+            'endTs': int(data['stop_timestamp']) * 1000,
+            'cameras': camera_query,
+            'skip': 0 if 'skip' not in data.keys() else data['skip'],
+            'limit': 10000 if 'limit' not in data.keys() else data['limit']
         }
 
         response = requests.get('http://{}:5005/archive_video'.format(self.default_serv.address), params=query_params)
