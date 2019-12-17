256ea33183503571148b51b503a159f5a79b608d
Egor14 <egorgrekhnev@mail.ru>
2019-12-17 15:20:20 +0300

    ptz absolute move fix

--- queue_api/ptz_control.py
+++ queue_api/ptz_control.py
@@ -4,15 +4,27 @@ import time
 from queue_api.messages import RequestParamValidationError
 from theorema.cameras.models import Camera
 
+from onvif import ONVIFCamera, ONVIFService
+
+def zeep_pythonvalue(self, xmlvalue):
+    return xmlvalue
+
 
 class PtzControlQueueEndpoint(QueueEndpoint):
 
     def move(self, x_coord, y_coord, zoom, address):
+        print(address, flush=True)
+        zeep.xsd.simple.AnySimpleType.pythonvalue = zeep_pythonvalue
         user = address.split('@')[0].split('/')[-1].split(':')[0]
         password = address.split('@')[0].split('/')[-1].split(':')[1]
         ip = address.split('@')[1].split('/')[0].split(':')[0]
-        port = address.split('@')[1].split('/')[0].split(':')[1]
+        try:
+            port = address.split('@')[1].split('/')[0].split(':')[1]
+            # mycam = ONVIFCamera(ip, port, user, password)
+        except:
+            port = 80
         mycam = ONVIFCamera(ip, port, user, password)
+
         # Create media service object
         media = mycam.create_media_service()
 
@@ -21,38 +33,37 @@ class PtzControlQueueEndpoint(QueueEndpoint):
 
         # Get target profile
         media_profile = media.GetProfiles()[0]
-
         # Get PTZ configuration options for getting continuous move range
         request = ptz.create_type('GetConfigurationOptions')
         request.ConfigurationToken = media_profile.PTZConfiguration.token
         ptz_configuration_options = ptz.GetConfigurationOptions(request)
-
+        # print(ptz_configuration_options, flush=True)
         move_request = ptz.create_type('AbsoluteMove')
         move_request.ProfileToken = media_profile.token
         # if move_request.Velocity is None:
-        move_request.Velocity = ptz.GetStatus({'ProfileToken': media_profile.token}).Position
+        move_request.Position = ptz.GetStatus({'ProfileToken': media_profile.token}).Position
+        print('current position', move_request.Position, flush=True)
 
-        # Get range of pan and tilt
-        # NOTE: X and Y are velocity vector
-        x_max = ptz_configuration_options.Spaces.ContinuousPanTiltVelocitySpace[0].XRange.Max
-        x_min = ptz_configuration_options.Spaces.ContinuousPanTiltVelocitySpace[0].XRange.Min
-        y_max = ptz_configuration_options.Spaces.ContinuousPanTiltVelocitySpace[0].YRange.Max
-        y_min = ptz_configuration_options.Spaces.ContinuousPanTiltVelocitySpace[0].YRange.Min
 
-        print(x_min, x_max, y_min, y_max, flush=True)
+        print(ptz_configuration_options, flush=True)
 
-        print('move ...')
-        move_request.Velocity.PanTilt.x += x_coord
-        move_request.Velocity.PanTilt.y += y_coord
-        move_request.Velocity.Zoom += zoom
+        print('move ...', flush=True)
 
-        # ptz.Stop({'ProfileToken': move_request.ProfileToken})
+
+        move_request.Position.PanTilt.x += x_coord
+        move_request.Position.PanTilt.y += y_coord
+        move_request.Position.Zoom.x += zoom
+
+        print('move_request', move_request, type(move_request), flush=True)
+
 
         ptz.AbsoluteMove(move_request)
 
         time.sleep(3)
-        # Stop continuous move
         ptz.Stop({'ProfileToken': move_request.ProfileToken})
+        move_request.Position = ptz.GetStatus({'ProfileToken': media_profile.token}).Position
+        print('new position', move_request.Position, flush=True)
+
 
 
 
@@ -79,7 +90,7 @@ class PanControlMessage(PtzControlQueueEndpoint):
         camera = Camera.objects.filter(uid=params['camera_id']).first()
         if camera:
             try:
-                self.move(int(params['step']), 0, 0, camera.address)
+                self.move(float(params['data']['step']), 0, 0, camera.address)
             except:
                 print('some error', flush=True)
                 error = RequestParamValidationError('camera with id {id} can not move'.format(id=params['camera_id']))
@@ -117,7 +128,7 @@ class TiltControlMessage(PtzControlQueueEndpoint):
         camera = Camera.objects.filter(uid=params['camera_id']).first()
         if camera:
             try:
-                self.move(0, int(params['step']), 0, camera.address)
+                self.move(0, float(params['data']['step']), 0, camera.address)
             except:
                 print('some error', flush=True)
                 error = RequestParamValidationError('camera with id {id} can not move'.format(id=params['camera_id']))
@@ -155,7 +166,7 @@ class ZoomControlMessage(PtzControlQueueEndpoint):
         camera = Camera.objects.filter(uid=params['camera_id']).first()
         if camera:
             try:
-                self.move(0, 0, int(params['step']), camera.address)
+                self.move(0, 0, float(params['data']['step']), camera.address)
             except:
                 print('some error', flush=True)
                 error = RequestParamValidationError('camera with id {id} can not move'.format(id=params['camera_id']))
