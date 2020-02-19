b3468342dee095a3ab2bf41a5e51919841a54fa2
Egor14 <egorgrekhnev@mail.ru>
2020-02-19 11:43:56 +0300

    del comment

--- queue_api/cameras_logs.py
+++ queue_api/cameras_logs.py
@@ -19,22 +19,4 @@ class LogsSendMessage(LogsQueueEndpoint):
         end_of_dict = params['data'].decode().index('}')
         data = json.loads(params['data'].decode()[:end_of_dict + 1])
 
-        # is_finished = data['isFinished'] if 'isFinished' in data else False
-
-        # message = {
-        #     'type': self.response_message_type,
-        #     'camera_id': data['archiveStartHint'].split('/')[1],
-        #     'data': {
-        #         'event_id': data['id'],
-        #         'event_start_timestamp': data['start_timestamp'],
-        #         'event_end_timestamp': data['end_timestamp'] if data['end_timestamp'] > 0 else 0,
-        #         'event_type': data['type'],
-        #         'confidence': data['confidence'],
-        #         'reaction': data['reaction'],
-        #         'is_finished': is_finished
-        #     }
-        # }
-
         base_send_in_queue(self.response_exchange, json.dumps(data))
-
-
